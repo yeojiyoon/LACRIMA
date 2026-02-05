65e298c31c12e002a4d4d0c7ff89acdd496222d9
여지윤 <81175074+yeojiyoon@users.noreply.github.com>
2026-02-05 20:31:24 +0900

    New Project

---
+++ src/main/java/com/lacrima/lacrimademo/LacrimaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lacrima.lacrimademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacrimaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LacrimaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lacrima/lacrimademo/LacrimaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacrima.lacrimademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacrimaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
